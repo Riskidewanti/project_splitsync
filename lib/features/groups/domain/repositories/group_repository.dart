09161c31177f24bb1bd536f379ed3a5ff9e048d8
Riski Dewanti <dewantiriski@gmail.com>
2026-06-12 16:25:13 +0700

    add group repository

---
+++ lib/features/groups/domain/repositories/group_repository.dart
@@ -0,0 +1,25 @@
+﻿import '../../data/models/group_member_model.dart';
+import '../../data/models/group_model.dart';
+
+abstract class GroupRepository {
+  Future<GroupModel> createGroup({
+    required String name,
+    String? description,
+  });
+
+  Future<List<GroupModel>> getUserGroups(String userId);
+
+  Future<GroupModel?> getGroupById(String groupId);
+
+  Future<GroupMemberModel> addMember({
+    required String groupId,
+    required String userId,
+    String? invitedBy,
+    GroupMemberRole role = GroupMemberRole.member,
+  });
+
+  Future<void> removeMember({
+    required String groupId,
+    required String userId,
+  });
+}
