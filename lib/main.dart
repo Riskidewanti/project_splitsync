3f0cabc9b561e3b3d86290883f436f28f29fea8b
salsa sabila <qrsal153@gmail.com>
2026-06-25 11:41:11 +0700

    reports

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+import 'reports/reports_page.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://mkdacnbbvjgekosdhevw.supabase.co/rest/v1/',
-    anonKey: 'sb_publishable_dpm-U61n41ih8DM8vGyNhQ_fMSyt5WL',
+    url: 'https://mkdacnbbvjgekosdhevw.supabase.co',
+    publishableKey: 'sb_publishable_dpm-U61n41ih8DM8vGyNhQ_fMSyt5WL',
   );
 
-print('Supabase Connected!');
-print(Supabase.instance.client.auth.currentUser);
   runApp(const MyApp());
 }
 
@@ -20,11 +20,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('SplitSync'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: ReportsPage(),
     );
   }
 }
